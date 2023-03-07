765897ce9ae0b8812e3515ccc2840bb3c57ad94a
ynisyfaa <yunisyafa8@gmail.com>
2023-03-07 13:25:34 +0700

    tugas 5

---
+++ tugas5.php
@@ -0,0 +1,6 @@
+<?php
+$perusahaan = ["toyota","daihatsu","honda","ford","ferari"];
+echo "perusahaan" .$perusahaan [4];
+echo "<br>";
+echo "perusahaan" .$perusahaan [1] ."dan" .$perusahaan [2];
+?>                       
